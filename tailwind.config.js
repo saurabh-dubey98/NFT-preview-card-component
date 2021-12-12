20a7cc30f282a1f3dc63746381b1101dd80006c6
saurabh <saurabhdubey8029@gmail.com>
2021-12-12 19:55:20 +0530

    first commit and added .gitignore

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ['./src/**/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        'main-bg': 'hsl(217, 54%, 11%)',
+        'card-bg': 'hsl(216, 50%, 16%)',
+        line: 'hsl(215, 32%, 27%)',
+        'soft-blue': 'hsl(215, 51%, 70%)',
+        cyan: 'hsl(178, 100%, 50%)'
+      },
+      fontFamily: {
+        body: ['Outfit']
+      }
+    },
+  },
+  plugins: [],
+}
